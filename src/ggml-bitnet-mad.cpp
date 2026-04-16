d9ffb24e7363e7796a684ae5a516f30cf45a787a
omnixtar <162043249+omnixtar@users.noreply.github.com>
2026-04-17 00:08:47 +0800

    Add files via upload

--- src/ggml-bitnet-mad.cpp
+++ src/ggml-bitnet-mad.cpp
@@ -808,8 +808,10 @@ void ggml_vec_dot_i2_i8_s_Nx1(int n, float * s, size_t bs, const void * vx, size
             accu[iy] = _mm256_setzero_si256();
         }
 
-        int8_t * y_col = y + col * by;
-        
+        // Omni 2026-03-24
+        // int8_t * y_col = y + col * by;
+        int8_t * y_col = const_cast<int8_t*>(y + col * by);
+
         for (int i = 0; i < group32_num; i++) {
             const uint8_t *px = x + i * 1024;
             const int8_t  *py = y_col + i * 4096;
